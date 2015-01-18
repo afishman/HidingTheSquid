16e461d3c38359ef09d54e9c360bc7da83ceccf3
Aaron Fishman <uk.afishman@gmail.com>
2015-01-17 16:48:16 -0800

    nmoving elements into their own folder

---
+++ Entities/Elements/FiberConstrainedElement.m
@@ -0,0 +1,26 @@
+classdef FiberConstrainedElement < Element
+    %FIBERCONSTRAINEDELEMENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+    end
+    
+    methods
+        function capacitance = Capacitance(this)
+            
+        end
+        
+        function width = Width(this)
+        end
+        
+        
+        function naturalWidth = NaturalWidth(this)
+        end
+        
+        function capDot = CapacitanceDot(this)
+        end
+    end
+    
+end
+

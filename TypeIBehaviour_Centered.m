dc802533e0b3d021e0b0f66b85009fbc9c1de2cd
Aaron Fishman <uk.afishman@gmail.com>
2015-02-22 17:20:13 -0800

    first go at centered flipflop

--- TypeIBehaviour_Centered.m
+++ TypeIBehaviour_Centered.m
@@ -1,13 +1,17 @@
-%These are optimal paramters
-%TODO: automate the creation of optimally spaced threads
+%Given spacing and a tail
+%clear all
+%
 preStretch = 2.5;
-nCells = 6;
+
 cellLengthAtPrestretch = 20e-3 * preStretch;
-spacingAtPrestretch = 80e-3 * preStretch;
-electrodeType = ElectrodeTypeEnum.LocallyControlled;
+resolution = 50e-3;
+tail = 3*resolution;
+spacingAtPrestretch = 3*resolution;
+
+interCellSpacing = 50e-3;
 
 %Define the switching models
-rOn = 2.4; rOff = 4.5;
+rOn = 2.4; rOff = 4.25;
 switchingModelLocal = TypeIModel(rOn, rOff);
 %switchingModelLocal = LocalAlwaysOffModel;
 
@@ -15,29 +19,23 @@ timeOn = 0; timeOff = 300;
 switchingModelExternal = StepModel(timeOn, timeOff);
 %switchingModelExternal = LocalAlwaysOffModel;
 
-%initialises a thread with equally spaced, locally controlled electrodes
-thread = Thread.ConstructThreadWithSpacedElectrodes( ...
-    preStretch, ...
-    cellLengthAtPrestretch, ...
-    nCells, ...
-    spacingAtPrestretch, ...
-    switchingModelLocal, ...
-    switchingModelExternal, ...
-    GentParams.Koh2012, ...
-    @(x)FiberConstrainedElement(x,1));
+stretchedLength = cellLengthAtPrestretch*2 + spacingAtPrestretch + 2*tail;
+elementConstructor = @(x) FiberConstrainedElement(x,1);
+thread = Thread(stretchedLength, resolution, preStretch, elementConstructor, GentParams.Koh2012);
+thread.SwitchingModelLocal = switchingModelLocal;
+thread.SwitchingModelExternal = switchingModelExternal;
 
-thread.Electrodes(6) = [];
-thread.Electrodes(5) = [];
-thread.Electrodes(3).Type = ElectrodeTypeEnum.ExternallyControlled;
-thread.Electrodes(4).Type = ElectrodeTypeEnum.LocallyControlled;
-thread.Electrodes(2) = [];
-thread.Electrodes(1) = [];
+%Add electrodes
+thread.AddElectrode(tail, cellLengthAtPrestretch, ElectrodeTypeEnum.LocallyControlled);
 
+secondElecrodeStart = tail + cellLengthAtPrestretch + spacingAtPrestretch;
+thread.AddElectrode(secondElecrodeStart, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
+
+%Find the driving voltage
+activatedCellStretch = 5;
+
+thread.RCCircuit.SourceVoltage = thread.CalculateDrivingVoltageForStretch(activatedCellStretch);
 thread.RCCircuit.Resistance = 1e8;
-return
-%Uncomment this to plot the thread! (it is currently in prestretch config)
-% showNodes = true;
-% sim.Thread.Plot(showNodes);
 
 %Make a simulator object and run for 7s
 simName = mfilename;

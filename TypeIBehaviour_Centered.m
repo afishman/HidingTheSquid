a757334a9bdca527ec3be7f8be03d8d5d89449c5
Aaron Fishman <uk.afishman@gmail.com>
2015-03-08 21:22:01 -0700

    solo centered type I

--- TypeIBehaviour_Centered.m
+++ TypeIBehaviour_Centered.m
@@ -1,46 +1,33 @@
 %Given spacing and a tail
 %clear all
 %
-preStretch = 2.7;
+preStretch = 2.5;
 
 cellLengthAtPrestretch = 20e-3 * 2.5;
 resolution = 50e-3;
-tail = 7*resolution;
-spacingAtPrestretch = 3*resolution;
-
-interCellSpacing = 50e-3;
+tail = 3*resolution;
 
 %Define the switching models
-rOn = 2.65; rOff = 4.5;
+rOn = 2.8; rOff = 4.2;
 switchingModelLocal = TypeIModel(rOn, rOff);
 %switchingModelLocal = LocalAlwaysOffModel;
 
-timeOn = 0; timeOff = 300;
+timeOn = 0; timeOff = 25;
 switchingModelExternal = StepModel(timeOn, timeOff);
 %switchingModelExternal = LocalAlwaysOffModel;
 
-stretchedLength = cellLengthAtPrestretch*2 + spacingAtPrestretch + 2*tail;
+stretchedLength = cellLengthAtPrestretch + 2*tail;
 elementConstructor = @(x) FiberConstrainedElement(x,1);
 thread = Thread(stretchedLength, resolution, preStretch, elementConstructor, GentParams.Koh2012);
 thread.SwitchingModelLocal = switchingModelLocal;
 thread.SwitchingModelExternal = switchingModelExternal;
 
 %Add electrodes
-thread.AddElectrode(tail, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
-
-%thread.AddElectrode(0*cellLengthAtPrestretch, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
-%thread.AddElectrode(1*cellLengthAtPrestretch, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
-%thread.AddElectrode(2*cellLengthAtPrestretch, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
-
-
-secondElecrodeStart = tail + cellLengthAtPrestretch + spacingAtPrestretch;
-%thread.AddElectrode(0, cellLengthAtPrestretch, ElectrodeTypeEnum.ExternallyControlled);
-thread.AddElectrode(secondElecrodeStart, cellLengthAtPrestretch, ElectrodeTypeEnum.LocallyControlled);
+thread.AddElectrode(tail, cellLengthAtPrestretch, ElectrodeTypeEnum.LocallyControlled);
 
 %Find the driving voltage
 activatedCellStretch = 5;
-
-thread.RCCircuit.SourceVoltage = thread.CalculateDrivingVoltageForStretch(activatedCellStretch);
+thread.RCCircuit.SourceVoltage = thread.DrivingVoltageForStretch(activatedCellStretch);
 thread.RCCircuit.Resistance = 1e8;
 
 close all
